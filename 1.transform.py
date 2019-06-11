1373a6bd94ea9b11e4124f7f250acb37b960a089
corgan <corganhejijun@qq.com>
2019-06-11 23:09:10 +0800

    celebA transform

--- 1.transform.py
+++ 1.transform.py
@@ -19,26 +19,27 @@ if not os.path.isdir(FRONT_DATA):
     os.mkdir(FRONT_DATA)
 
 totalLength = str(len(os.listdir(DATASET)))
-for index, subFolder in enumerate(os.listdir(DATASET)):
-    print("processing " + subFolder + " " + str(index+1) + " of total " + totalLength)
-    subFolderPath = os.path.join(DATASET, subFolder)
-    totalSubFile = str(len(os.listdir(subFolderPath)))
-    for i, imgFile in enumerate(os.listdir(subFolderPath)):
-        print("processing " + imgFile + " " + str(i+1) + " of total " + totalSubFile 
-                + " in subFolder " + subFolder + " " + str(index+1) + "/" + str(totalLength))
-        if not REPLACE_OLD and os.path.exists(os.path.join(destDir, imgFile)):
+#for index, subFolder in enumerate(os.listdir(DATASET)):
+#    print("processing " + subFolder + " " + str(index+1) + " of total " + totalLength)
+#    subFolderPath = os.path.join(DATASET, subFolder)
+#    totalSubFile = str(len(os.listdir(subFolderPath)))
+#    for i, imgFile in enumerate(os.listdir(subFolderPath)):
+for i, imgFile in enumerate(os.listdir(DATASET)):
+    print("processing " + imgFile + " " + str(i+1) + " of total " + totalLength)
+#                + " in subFolder " + subFolder + " " + str(index+1) + "/" + str(totalLength))
+    if not REPLACE_OLD and os.path.exists(os.path.join(destDir, imgFile)):
+        continue
+    imgPath = os.path.join(DATASET, imgFile)
+    img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
+    frontImg = landmark.copyFront(img)
+    if frontImg is None:
+        transImg = landmark.landmark_transform(img)
+        if transImg is None:
+            print("transform failed: file %s has no face" % imgFile)
             continue
-        imgPath = os.path.join(subFolderPath, imgFile)
-        img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
-        frontImg = landmark.copyFront(img)
-        if frontImg is None:
-            transImg = landmark.landmark_transform(img)
-            if transImg is None:
-                print("transform failed: file %s has no face" % imgFile)
-                continue
-            resizeImg = misc.imresize(transImg, (OUT_SIZE, OUT_SIZE))
-            misc.imsave(os.path.join(destDir, imgFile), resizeImg)
-        else:
-            resizeImg = misc.imresize(frontImg, (OUT_SIZE, OUT_SIZE))
-            misc.imsave(os.path.join(FRONT_DATA, imgFile), resizeImg)
+        resizeImg = misc.imresize(transImg, (OUT_SIZE, OUT_SIZE))
+        misc.imsave(os.path.join(destDir, imgFile), resizeImg)
+    else:
+        resizeImg = misc.imresize(frontImg, (OUT_SIZE, OUT_SIZE))
+        misc.imsave(os.path.join(FRONT_DATA, imgFile), resizeImg)
         
