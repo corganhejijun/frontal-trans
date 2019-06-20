f736548338179913cb3cec1c1d85b640046b74b4
corgan <corganhejijun@qq.com>
2019-06-20 13:25:55 +0800

    bug fix

--- test_sample.py
+++ test_sample.py
@@ -21,7 +21,7 @@ for index, file in enumerate(fileList):
     print("procesing " + file + " " + str(index+1) + '/' + str(len(fileList)))
     img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
     img = misc.imresize(img, (imgSize, imgSize), interp='bilinear')
-    img = cv2.GaussianBlur(img, sigma)
+    img = cv2.GaussianBlur(img, kernel_size, sigma)
     combineImg = Image.new('RGB', (img.shape[0]*2, img.shape[0]))
     combineImg.paste(Image.fromarray(img), (0,0))
     combineImg.paste(Image.fromarray(img), (img.shape[0]+1,0))
