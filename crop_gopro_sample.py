5000bf14521bdb00173a82efc2abd8e50bc14bce
corgan hejijun <corganhejijun@gmail.com>
2019-08-13 23:55:04 +0800

    corp gopro test to 1280x720

---
+++ crop_gopro_sample.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import os
+from PIL import Image
+from scipy import misc
+
+combine_path = 'gopro_combine'
+result_path = 'gopro_crop'
+
+if not os.path.isdir(result_path):
+  os.mkdir(result_path)
+
+for file in os.listdir(combine_path):
+  img = Image.open(os.path.join(combine_path, file))
+  misc.imsave(os.path.join(result_path, file),
+              img.crop(0,0,1280,720))
