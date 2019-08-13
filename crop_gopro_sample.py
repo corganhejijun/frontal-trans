d1108de01630818ae9293f7aa9fb068c2a5a36c9
corgan hejijun <corganhejijun@gmail.com>
2019-08-13 23:56:47 +0800

    bug fix

--- crop_gopro_sample.py
+++ crop_gopro_sample.py
@@ -10,6 +10,7 @@ if not os.path.isdir(result_path):
   os.mkdir(result_path)
 
 for file in os.listdir(combine_path):
+  print('processing ' + file)
   img = Image.open(os.path.join(combine_path, file))
   misc.imsave(os.path.join(result_path, file),
-              img.crop(0,0,1280,720))
+              img.crop((0,0,1280,720)))
