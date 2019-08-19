a6e73f4023dcea75a6e189d785597d52aeb27a6b
corgan hejijun <corganhejijun@gmail.com>
2019-08-19 21:08:22 +0800

    fix some problems

--- get_lfw_test_sample.py
+++ get_lfw_test_sample.py
@@ -6,8 +6,8 @@ from PIL import Image
 
 sample_path = 'datasets/lfw'
 dest_path = 'datasets/lfw_test'
-middleSize = 64
-imgSize = 256
+middleSize = 32
+imgSize = 128
 kernel_size = (5, 5)
 sigma = 5
 
@@ -22,11 +22,10 @@ for index, subFolder in enumerate(lfw_list):
         imgPath = os.path.join(sample_path, subFolder, file)
         if os.path.isdir(imgPath):
             continue
-        img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
-        img = misc.imresize(img, (middleSize, middleSize), interp='bilinear')
-        img = misc.imresize(img, (imgSize, imgSize), interp='bilinear')
-        img = cv2.GaussianBlur(img, kernel_size, sigma)
+        imgB = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
+        imgA = misc.imresize(imgB, (middleSize, middleSize), interp='bilinear')
+        imgA = misc.imresize(imgA, (imgSize, imgSize), interp='bilinear')
         combineImg = Image.new('RGB', (img.shape[0]*2, img.shape[0]))
-        combineImg.paste(Image.fromarray(img), (0,0))
-        combineImg.paste(Image.fromarray(img), (img.shape[0]+1,0))
+        combineImg.paste(Image.fromarray(imgA), (0,0))
+        combineImg.paste(Image.fromarray(imgB), (img.shape[0]+1,0))
         misc.imsave(os.path.join(dest_path, file), combineImg)
