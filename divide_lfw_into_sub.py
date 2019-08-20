b5758e07c857500e604a486aa5d9531b36360b95
corgan hejijun <corganhejijun@gmail.com>
2019-08-20 21:39:17 +0800

    combine gt and sample in to one folder

--- divide_lfw_into_sub.py
+++ divide_lfw_into_sub.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*- 
 import os
 
-lfw_test_path = 'test/'
-dest_path = 'lfw_ours_128'
+lfw_data_path = 'compare/lfw_128/test'
+dest_path = 'compare/lfw_128/test_'
+gt_path = 'compare/lfw_128/gt'
 
 if not os.path.exists(dest_path):
     os.mkdir(dest_path)
 
-fileList = os.listdir(lfw_test_path)
+fileList = os.listdir(lfw_data_path)
 for index, file in enumerate(fileList):
   print("processing " + file + " " + str(index) + " of total " + str(len(fileList)))
   # name: 'Tony_Blair_0114.png'
-  subFolderName = file[:-9]
+  subFolderName = file[:-4]
   subFolderPath = os.path.join(dest_path, subFolderName)
-  if not os.path.isdir(subFolderPath):
-    os.mkdir(subFolderPath)
+  os.mkdir(subFolderPath)
   os.rename(os.path.join(lfw_test_path, file), os.path.join(subFolderPath, file))
+  os.rename(os.path.join(gt_path, file), os.path.join(subFolderName, file[:-4] + '_gt' + file[-4:]))
